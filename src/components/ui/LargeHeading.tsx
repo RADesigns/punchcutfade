f88281b8daa9bcb12a49c1a6eb2971aad394921e
ryan anderson <ryanmanderson1986@gmail.com>
2023-04-22 21:45:57 -0500

    updated import aliases for ui

--- src/components/ui/LargeHeading.tsx
+++ src/components/ui/LargeHeading.tsx
@@ -1,6 +1,6 @@
 import { type FC } from 'react'
 
-import { cn } from '~/utils/cn'
+import { cn } from '@/utils/cn'
 import { cva, type VariantProps } from 'class-variance-authority'
 
 const headingVariants = cva(
